251d134157ae9a7ef5df1ba801d9866d30b40ab2
Oliver Wilkes <owilkes@SOCNET.SOC.PLYMOUTH.AC.UK>
2020-02-13 12:50:33 +0000

    added colorconverter example

---
+++ ColorConverter/ColorConverter/ColorConverter/ColorConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ColorConverter
+{
+    class ColorConverter : IValueConverter
+    {
+
+        //Implement this method to convert value to targetType by using parameter and culture.
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int v = (int)value;
+            Color c;
+            switch (v)
+            {
+                case 0:
+                    c = Color.Red;
+                    break;
+                case 1:
+                    c = Color.Gold;
+                    break;
+                case 2:
+                    c = Color.Green;
+                    break;
+                default:
+                    c = Color.Black;
+                    break;
+            }
+            return c;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ColorConverter/ColorConverter/ColorConverter/Model.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ColorConverter
+{
+    class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<string> Sayings = new List<string>
+        {
+            "May the Force be With You",
+            "Live long and prosper",
+            "Nanoo nanoo"
+        };
+
+        //Index of which saying to use
+        private int next = 0;
+        public int SayingNumber
+        {
+            get => next;
+            set
+            {
+                if (next != value)
+                {
+                    next = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("SayingNumber"));
+                    }
+                }
+            }
+        }
+        string _currentSaying = "Welcome to Xamarin Forms!";
+        public string CurrentSaying
+        {
+            get => _currentSaying;
+            set
+            {
+                if (!value.Equals(_currentSaying))
+                {
+                    _currentSaying = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("CurrentSaying"));
+                    }
+                }
+            }
+        }
+
+        public void NextMessage()
+        {
+            SayingNumber = (SayingNumber + 1) % Sayings.Count;
+            CurrentSaying = Sayings[SayingNumber];
+        }
+
+        bool _visible = true;
+        public bool IsTrue
+        {
+            get => _visible;
+            set
+            {
+                if (value != _visible)
+                {
+                    _visible = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("IsTrue"));
+                    }
+                }
+            }
+        }
+
+
+        public Model()
+        {
+            _currentSaying = Sayings[0];
+        }
+
+    }
+}
